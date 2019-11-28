eca42ff5ec55d625d1795a4742218192d8ab7660
Take-Hiro-Masa <n65.hirose.masatoshi@gmail.com>
2019-11-28 16:12:59 +0900

    4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!, except: [:new, :create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def authenticate_user!
+    redirect_to new_user_session_path unless user_signed_in?
+  end
 end
